ce71352ff263b3a3b831cf8b3f25ae0108d7a3b5
Kato <kubkate@gmail.com>
2020-08-10 22:26:19 -0700

    Initail setup Navbar with github icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello App!</h1>
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
       </div>
     );
   }
