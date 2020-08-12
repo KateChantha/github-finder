a1930c000684f85867862984b72b19230d198f98
Kato <kubkate@gmail.com>
2020-08-11 20:51:01 -0700

    Create UserItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
+        <UserItem />
       </div>
     );
   }
