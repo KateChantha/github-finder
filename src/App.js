c948452810dc65011a0360d21c7cbbc94e35d24b
Kato <kubkate@gmail.com>
2020-08-11 22:03:59 -0700

    Lift state up from Users to App, fetch github user in App and pass props to Users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
