a767e798edf1e18b68b8e10043f14bc59ec46bab
Kato <kubkate@gmail.com>
2020-08-11 22:26:06 -0700

    Create Spinner, Refactor Users from class-base to functional and add logic for loading spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-class Users extends React.Component {
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
   }
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 // in-line style 
