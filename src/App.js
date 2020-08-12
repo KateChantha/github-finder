f0df0d652e7b5a39eb5db1195f2d3efbf19f1f2d
Kato <kubkate@gmail.com>
2020-08-12 14:54:32 -0700

    Cleanup note, add propTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
@@ -12,7 +14,11 @@ class App extends React.Component {
     loading: false
   }
 
-  // // ---- fetch users when page loaded ---
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  }
+
+  // ======= fetch initail users when page is loaded ========
   // async componentDidMount() {
   //   this.setState({ loading: true })
   //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
@@ -20,8 +26,15 @@ class App extends React.Component {
   //   this.setState({ users: res.data, loading: false })
   // }
 
-  // call to github api
+  /**
+   * @desc endpiont at /search/users?q=${text}
+   * @request-> text ; search 'text' submit call query to github api 
+   * @response-> res.data.items
+   */
   searchUsers = async (text) => {
+    // load spinner
+    this.setState({ loading: true })
+
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ users: res.data.items, loading: false })
@@ -39,5 +52,6 @@ class App extends React.Component {
     );
   }
 }
+
 
 export default App;
