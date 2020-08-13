90139fce6e5c2e9602e14161b6584c126279b576
Kato <kubkate@gmail.com>
2020-08-13 15:42:42 -0700

    Convert User from class to functional and replaced compoenentDidMount by useEffect

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,114 +1,113 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 
-// Class-base to access componentDidMount
-class User extends React.Component {
-  componentDidMount() {
-    /* REFERENCE FORM App.js ******
+
+const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+  /* REFERENCE FORM App.js ******
     <Route 
       exact path='/user/:login' <==== params.login =====
       render={props => (
       <User {...props} getUser={this.getUser} user={user} loading={loading} />
     )} /> 
     */
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired
-  }
+  // replace componentdidMount using useEffect hook. 
+  // Have to pass in [] as 2nd argument to stop the on going update loop -- so it will run only once 
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, [])
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      login,
-      blog,
-      html_url,
-      company,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user;
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    login,
+    blog,
+    html_url,
+    company,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+  } = user;
 
-    const { loading, repos } = this.props;
-
-    if (loading) {
-      return <Spinner />
-    } else {
-      return (
-        <Fragment>
-          <Link to='/' className='btn btn-light' >
-            Back To Search
+  if (loading) {
+    return <Spinner />
+  } else {
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light' >
+          Back To Search
           </Link>
-          <div className='card grid-2'>
-            <div className='all-center'>
-              <img
-                src={avatar_url}
-                className='round-img'
-                alt=''
-                style={{ width: '150px' }}
-              />
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
 
-              <h1>{name}</h1>
-              <p>Locaton: {location}</p>
-            </div>
-            <div>
-              {bio && (
-                <Fragment>
-                  <h3>Bio</h3>
-                  <p>{bio}</p>
-                </Fragment>
-              )}
-              <a href={html_url} className='btn btn-dark my-1' >
-                Visit Github Profile
+            <h1>{name}</h1>
+            <p>Locaton: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1' >
+              Visit Github Profile
               </a>
-              <ul>
-                <li>
-                  {login && <Fragment>
-                    <strong>Username: </strong>{login}
-                  </Fragment>}
-                </li>
-                <li>
-                  {company && <Fragment>
-                    <strong>Company: </strong>{company}
-                  </Fragment>}
-                </li>
-                <li>
-                  {blog && <Fragment>
-                    <strong>Website: </strong>{blog}
-                  </Fragment>}
-                </li>
-              </ul>
-            </div>
+            <ul>
+              <li>
+                {login && <Fragment>
+                  <strong>Username: </strong>{login}
+                </Fragment>}
+              </li>
+              <li>
+                {company && <Fragment>
+                  <strong>Company: </strong>{company}
+                </Fragment>}
+              </li>
+              <li>
+                {blog && <Fragment>
+                  <strong>Website: </strong>{blog}
+                </Fragment>}
+              </li>
+            </ul>
           </div>
+        </div>
 
-          <div className='card text-center'>
-            <div className='badge badge-primary'>Followers: {followers}</div>
-            <div className='badge badge-success'>Following: {following}</div>
-            <div className='badge badge-light'>Public Repos: {public_repos}</div>
-            <div className='badge badge-dark'>Public Gists: {public_gists}</div>
-          </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
 
-          <Repos repos={repos} />
+        <Repos repos={repos} />
 
-        </Fragment>
-      )
-    }
+      </Fragment>
+    )
   }
+}
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired
 }
 
 export default User;
